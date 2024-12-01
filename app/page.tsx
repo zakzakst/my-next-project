2f5b0c6a9aa49ebc28c0556465a10d23da650768
zakzakst <e1433104@gmail.com>
2024-12-01 21:44:06 +0900

    3-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   const name = "世界";
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
+    </section>
   );
 }
