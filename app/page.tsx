454e6b2dcb191271d59549c58834a206d98be3f9
zakzakst <e1433104@gmail.com>
2024-12-06 23:47:56 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,33 @@ export default function Home() {
         <h2 className={styles.newsTitle}>News</h2>
         <ul>
           {data.contents.map((article) => (
-            <li key={article.id}>{article.title}</li>
+            <li key={article.id} className={styles.list}>
+              <div className={styles.link}>
+                <Image
+                  className={styles.image}
+                  src="/no-image.png"
+                  alt="No Image"
+                  width={1200}
+                  height={630}
+                />
+                <dl className={styles.content}>
+                  <dt className={styles.newsItemTitle}>{article.title}</dt>
+                  <dd className={styles.meta}>
+                    <span className={styles.tag}>{article.category.name}</span>
+                    <span className={styles.date}>
+                      <Image
+                        src="/clock.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        priority
+                      />
+                      {article.publishedAt}
+                    </span>
+                  </dd>
+                </dl>
+              </div>
+            </li>
           ))}
         </ul>
       </section>
