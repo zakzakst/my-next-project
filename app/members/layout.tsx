1f906d007a0e0dd6ccd7d65464d05e47c8531e54
zakzakst <e1433104@gmail.com>
2024-12-12 23:43:14 +0900

    修正

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,9 +1,15 @@
 import Sheet from "../_components/Sheet"
+import Hero from "../_components/Hero"
 
 type Props = {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: Props) {
-  return <Sheet>{children}</Sheet>
+  return (
+    <>
+      <Hero title="Members" sub="メンバー" />
+      <Sheet>{children}</Sheet>
+    </>
+  )
 }
