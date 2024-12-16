6e00a3a909e5e74450f0b608632e36db1a950570
zakzakst <e1433104@gmail.com>
2024-12-16 11:59:22 +0900

    修正

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/app/_components/Hero";
 import Sheet from "@/app/_components/Sheet";
+import { Metadata } from "next";
 
 type Props = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: "ニュース",
 };
 
 export default function NewsLayout({ children }: Props) {
