1fc191234017468828f6a64e7db4df22d66a2760
zakzakst <e1433104@gmail.com>
2024-12-17 10:34:04 +0900

    修正

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
@@ -12,6 +13,27 @@ type Props = {
     dk?: string;
   };
 };
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const { dk } = await searchParams;
+  const data = await getNewsDetail(slug, {
+    draftKey: dk,
+  });
+
+  return {
+    title: data.title,
+    description: data.description,
+    openGraph: {
+      title: data.title,
+      description: data.description,
+      images: [data?.thumbnail?.url ?? ""],
+    },
+  };
+}
 
 export default async function Page({ params, searchParams }: Props) {
   // 下記を参考に書籍の内容を修正
